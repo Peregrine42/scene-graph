7853863413ae02c3c05b3cad71c9a327b30ffce1
Duncan Fermor <duncan@elitemail.org>
2015-10-10 04:16:56 +0100

    refactoring 1

--- public/wait_for.js
+++ public/wait_for.js
@@ -1,9 +1,17 @@
 var nester = require('./nester.js');
 
-var wait_for = function(func) {
+var wait_for = function() {
+  var array = Array.prototype.slice.call(arguments);
+  var func = array.shift();
   var deferrer = {};
-  deferrer.chain = [func];
-  deferrer.and_call = function(func) {
+  deferrer.chain = [];
+  var new_func = function() {
+    var args = Array.prototype.slice.call(arguments);
+    var new_args = args.concat(array);
+    return func.apply(null, new_args)
+  };
+  deferrer.chain.push(new_func);
+  deferrer.and_call = function() {
     var array = Array.prototype.slice.call(arguments);
     var func = array.shift();
     var new_func = function() {
